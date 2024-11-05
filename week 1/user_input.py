c5bd719f3ba9a8aed296e9d4dc712510048d5f00
Mr-kings042 <okohkings042@gmail.com>
2024-11-05 16:16:47 +0100

    first commit

---
+++ week 1/user_input.py
@@ -0,0 +1,7 @@
+# get user inputs
+name = input("What is your name? ") 
+age = input("What is your age? ")
+location = input("What is your location? ")
+
+# Print the user inputs
+print(f"Hello, {name}! You are {age} years old and live in {location}")
